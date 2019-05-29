361461a7f038884d8ebe1aa298f71f3e6aee2f1b
Hxx1221 <137125668@qq.com>
2019-05-29 20:45:57 +0800

    54654

---
+++ src/main/java/Thread/Test.java
@@ -0,0 +1,11 @@
+package Thread;
+
+/**
+ * @Author:He_xixiang
+ * @Title: ${enclosing_method}
+ * @Description: ${todo}(这里用一句话描述这个方法的作用)
+ * @return ${return_type}    返回类型
+ * @throws
+ */
+public class Test {
+}
